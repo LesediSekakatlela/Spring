5b0db7c024ff7c0b5b056a82cb35a4b1df0d9e08
Lesedi <leseditumelo32@gmail.com>
2022-01-31 16:33:51 +0200

    Creating components

--- React and Spring/todo-app/src/App.js
+++ React and Spring/todo-app/src/App.js
@@ -7,8 +7,33 @@ class App extends Component {
     return (
       <div className="App">
         My Hello World
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
       </div>
     );
   }
 }
+
+//Class Component
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="firstComponent">
+        FirstComponent
+      </div>
+    );
+  }
+}
+
+//Class Component
+class SecondComponent extends Component {
+  render() {
+    return (
+      <div className="secondComponent">
+        SecondComponent
+      </div>
+    );
+  }
+}
+
 export default App;

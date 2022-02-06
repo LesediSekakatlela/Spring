560608bdedb1b191cfdf72c8a650cfd6b4313872
Lesedi <leseditumelo32@gmail.com>
2022-02-06 18:58:49 +0200

    building counter and button

--- React and Spring/todo-app/src/components/counter/Counter.jsx
+++ React and Spring/todo-app/src/components/counter/Counter.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import './Counter.css';
 
 function Counter() {
     return (
         <div className="counter">
-            Counter
+            <button onClick={increment}>+1</button>
+            <span className="count">0</span>
         </div>
     );
+}
+
+function increment() {
+    console.log('increment');
 }
 
 export default Counter;

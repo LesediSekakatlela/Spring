74c34e51dd1dded86c2e095fc015c38112e9dc0c
Lesedi <leseditumelo32@gmail.com>
2022-02-07 19:50:20 +0200

    adding state

--- React and Spring/todo-app/src/components/counter/Counter.jsx
+++ React and Spring/todo-app/src/components/counter/Counter.jsx
@@ -3,23 +3,42 @@ import PropTypes from 'prop-types';
 import './Counter.css';
 
 class Counter extends Component {
+
+    constructor() {
+        super(); //Error 1
+        this.state = {
+            counter : 0
+        }
+
+        this.increment = this.increment.bind(this);
+    }
+
     render() {
         return (
-          <div className="counter">
-            <CounterButton by={1}/>
-            <CounterButton by={5}/>
-            <CounterButton by={10}/>
-          </div>
+            <div className="counter">
+                <CounterButton by={1} incrementMethod={this.increment}/>
+                <CounterButton by={5} incrementMethod={this.increment}/>
+                <CounterButton by={10} incrementMethod={this.increment}/>
+                <span className="count">{this.state.counter}</span>
+            </div>
         );
-      }
+    }
+
+    increment(by) { 
+        // console.log(`increment from child - ${by}`)
+        this.setState({
+            counter: this.state.counter + by
+        });
+    }
 }
+
 class CounterButton extends Component {
     //Define the initial state in a constructor
     //state => counter 0
     constructor() {
         super(); //Error 1
         this.state = {
-            counter : 0,
+            counter : 0
         }
 
         this.increment = this.increment.bind(this);
@@ -41,8 +60,9 @@ class CounterButton extends Component {
         //console.log('increment');
         //this.state.counter++; //Bad Practice
         this.setState({
-            counter: this.state.counter + this.props.by
+            counter : this.state.counter + this.props.by
         });
+        this.props.incrementMethod(this.props.by);
     }
 }
 

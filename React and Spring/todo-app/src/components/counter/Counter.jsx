a57176355de4986460b18d91efdc3af11f82788f
Lesedi <leseditumelo32@gmail.com>
2022-02-07 19:02:20 +0200

    counter and counterButton

--- React and Spring/todo-app/src/components/counter/Counter.jsx
+++ React and Spring/todo-app/src/components/counter/Counter.jsx
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 import './Counter.css';
 
 class Counter extends Component {
+    render() {
+        return (
+          <div className="counter">
+            <CounterButton by={1}/>
+            <CounterButton by={5}/>
+            <CounterButton by={10}/>
+          </div>
+        );
+      }
+}
+class CounterButton extends Component {
     //Define the initial state in a constructor
     //state => counter 0
     constructor() {
@@ -35,11 +46,11 @@ class Counter extends Component {
     }
 }
 
-Counter.defaultProps = {
+CounterButton.defaultProps = {
     by : 1
 }
 
-Counter.propTypes = {
+CounterButton.propTypes = {
     by : PropTypes
 }
 

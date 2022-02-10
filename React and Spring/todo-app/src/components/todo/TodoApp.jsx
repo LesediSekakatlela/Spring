4c44d4cf593690e449f8737ae715cc22282f28c4
Lesedi <leseditumelo32@gmail.com>
2022-02-10 20:27:37 +0200

    enable menu

--- React and Spring/todo-app/src/components/todo/TodoApp.jsx
+++ React and Spring/todo-app/src/components/todo/TodoApp.jsx
@@ -33,17 +33,19 @@ class TodoApp extends Component {
 
 class HeaderComponent extends Component {
     render() {
+        const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
+        // console.log(isUserLoggedIn);
         return (
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <div><a href="http://www.in28minutes.com" className="navbar-brand">in28Minutes</a></div>
                     <ul className="navbar-nav">
-                        <li><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
-                        <li><Link className="nav-link" to="/todos">Todos</Link></li>
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/todos">Todos</Link></li>}
                     </ul>
                     <ul className="navbar-nav navbar-collapse justify-content-end">
-                        <li><Link className="nav-link" to="/login">Login</Link></li>
-                        <li><Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Logout</Link></li>
+                        {!isUserLoggedIn && <li><Link className="nav-link" to="/login">Login</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Logout</Link></li>}
                     </ul>
                 </nav>
             </header>
@@ -95,7 +97,7 @@ class ListTodosComponent extends Component {
                     <table className="table">
                         <thead>
                             <tr>
-                                <th>description</th>
+                                <th>Description</th>
                                 <th>Target Date</th>
                                 <th>Is Completes?</th>
                             </tr>
@@ -104,7 +106,7 @@ class ListTodosComponent extends Component {
                             {
                                 this.state.todos.map(
                                     todo =>
-                                        <tr>
+                                        <tr key={todo.id}>
                                             <td>{todo.description}</td>
                                             <td>{todo.done.toString()}</td>
                                             <td>{todo.targetDate.toString()}</td>

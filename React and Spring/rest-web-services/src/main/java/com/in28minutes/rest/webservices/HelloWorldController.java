5a56d36230c726263a340898cf8899b832efed08
Lesedi <leseditumelo32@gmail.com>
2022-02-12 11:52:09 +0200

    Creating a Spring Boot Hello World Service

---
+++ React and Spring/rest-web-services/src/main/java/com/in28minutes/rest/webservices/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller 
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping( path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	//
+	@GetMapping( path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}

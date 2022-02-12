dae61c5a8c57b74e250af3c77156d9270d8468c7
Lesedi <leseditumelo32@gmail.com>
2022-02-12 12:16:27 +0200

    Enhancing the Spring Boot Hello World Service to return a Bean

--- React and Spring/rest-web-services/src/main/java/com/in28minutes/rest/webservices/HelloWorldController.java
+++ React and Spring/rest-web-services/src/main/java/com/in28minutes/rest/webservices/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.in28minutes.rest.webservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller 
@@ -14,10 +12,10 @@ public class HelloWorldController {
 		return "Hello World";
 	}
 	
-	//
-	@GetMapping( path = "/hello-world")
-	public String helloWorld() {
-		return "Hello World";
+	@GetMapping( path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
+
 
 }

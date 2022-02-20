626aeadc8d514a416fe9c19cf34ed3c61c92854e
Lesedi <leseditumelo32@gmail.com>
2022-02-20 14:50:00 +0200

    Initializing and Handling Form Submit with Formik

--- React and Spring/todo-app/src/components/todo/TodoComponent.jsx
+++ React and Spring/todo-app/src/components/todo/TodoComponent.jsx
@@ -8,24 +8,36 @@ class TodoComponent extends Component {
 
         this.state = {
             id: this.props.params.id,
-            description: '',
+            description: 'Learn Forms',
             targetDate: moment(new Date()).format('YYYY-MM-DD')
         }
+
+        this.onSubmit = this.onSubmit.bind(this)
+        // this.validate = this.validate.bind(this)
+    }
+
+    onSubmit(values) {
+        console.log(values);
     }
 
 
     render() {
+        let { description, targetDate } = this.state
+
         return (
             <div>
                 <h1>Todo</h1>
                 <div className="container">
-                    <Formik>
+                    <Formik
+                        initialValues={{ description, targetDate }}
+                        onSubmit={this.onSubmit}
+                        >
                         {
                             (props) => (
                                 <Form>
                                     <fieldset className="form-group">
                                         <label>Description</label>
-                                        <Field className="form-control" type="text" name="description"/>
+                                        <Field className="form-control" type="text" name="description" />
                                     </fieldset>
                                     <fieldset className="form-group">
                                         <label>Target Date</label>

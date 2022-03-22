ec881c20c6ff78cd3411f297f08faa1bafe08dbc
Lesedi <leseditumelo32@gmail.com>
2022-03-22 20:22:30 +0200

    Auth Header

--- React and Spring/todo-app/src/api/todo/HelloWorldService.js
+++ React and Spring/todo-app/src/api/todo/HelloWorldService.js
@@ -17,14 +17,14 @@ class HelloWorldService {
         //let username = 'in28minutes'
         //let password = 'dummy'
 
-        //let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
+        let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
 
         return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`
-        //  ,{
-        //   headers : {
-        //       authorization: basicAuthHeader
-        //      }
-        //   }
+         ,{
+          headers : {
+              authorization: basicAuthHeader
+             }
+          }
         );
     }
 }

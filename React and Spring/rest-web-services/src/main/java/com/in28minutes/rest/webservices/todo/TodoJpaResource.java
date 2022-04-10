4b9e4798902fdbad5df250c26b3b965564448d78
Lesedi <leseditumelo32@gmail.com>
2022-04-10 17:51:04 +0200

    adding todo username

--- React and Spring/rest-web-services/src/main/java/com/in28minutes/rest/webservices/todo/TodoJpaResource.java
+++ React and Spring/rest-web-services/src/main/java/com/in28minutes/rest/webservices/todo/TodoJpaResource.java
@@ -43,27 +43,24 @@ public class TodoJpaResource {
 	//DELETE /users/{username}/todos/{id}
 	@DeleteMapping("/jpa/users/{username}/todos/{id}")
 	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
-		Todo todo = todoService.deleteById(id);
-		if(todo!=null) {
-			return ResponseEntity.noContent().build();
-		}
 		
-		return ResponseEntity.notFound().build();
+		todoJpaRepository.deleteById(id);
+			return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping("/jpa/users/{username}/todos/{id}")
-	public ResponseEntity<Todo> updateTodo(@PathVariable String username, 
-			@PathVariable long id, 
-			@RequestBody Todo todo) {
-		Todo todoUpdated = todoService.save(todo);
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo) {
+		
+		todo.setUsername(username);
+		Todo todoUpdated = todoJpaRepository.save(todo);
 		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
 	}
 	
 	@PostMapping("/jpa/users/{username}/todos")
-	public ResponseEntity<Void> updateTodo(
-			@PathVariable String username, @RequestBody Todo todo) {
+	public ResponseEntity<Void> createTodo(@PathVariable String username, @RequestBody Todo todo) {
 		
-		Todo createdTodo = todoService.save(todo);
+		todo.setUsername(username);
+		Todo createdTodo = todoJpaRepository.save(todo);
 		
 		//Location 
 		//Get current resource url
